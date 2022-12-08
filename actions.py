840e4ded9777eee3581dc0cb7645dd6ab141b754
Zach Derish <zderish@gmail.com>
2022-12-08 16:55:54 -0500

    Add files via upload

--- actions.py
+++ actions.py
@@ -1,13 +1,14 @@
 import random
 import graphics
 
-graphs = [[["0" for y in range (100)] for x in range(50)] for i in range(10)]
+graphs = [[["0" for y in range (50)] for x in range(100)] for i in range(10)]
 
 
+# generate graphs
 def genGraph():
     for i in range(10):
-        for x in range(50):
-            for y in range(100):
+        for x in range(100):
+            for y in range(50):
                 num = random.random()
                 if(num <= .5):
                     graphs[i][x][y] = "N"
@@ -19,6 +20,7 @@ def genGraph():
                     graphs[i][x][y] = "B"
 
 
+# generate readings
 def reading(location):
     num = random.random()
     if location == 'N':
@@ -44,23 +46,22 @@ def reading(location):
             return 'H'
 
 
+# get random next move
 def nextMove():
     move = random.randint(1,4)
-    moveOdds = random.random()
-    if(moveOdds >= .9):
-        move = 0
-        return move
-    else:
-        return move
+    return move
 
+# generate path
 def sequence(start, j):
     readings = [None] * 100
     actions = [None] * 100
     sequence = [None] * 100
-    for i in range(99):
+    for i in range(100):
         move = nextMove()
+        # determine whether move is actually enacted
+        moveOdds = random.random()
         #west move
-        if (move == 1):
+        if (move == 1 and moveOdds <= .9):
             actions[i] = 'L'
             if(start[0] == 0):
                 sequence[i] = start
@@ -74,7 +75,7 @@ def sequence(start, j):
                 start = ((start[0]-1),start[1])
 
         #north move
-        elif (move == 2):
+        elif (move == 2 and moveOdds <= .9):
             actions[i] = 'U'
             if(start[1] == 0):
                 sequence[i] = start
@@ -88,9 +89,9 @@ def sequence(start, j):
                 start = (start[0],(start[1]-1))
         
         #east move 
-        elif (move == 3):
+        elif (move == 3 and moveOdds <= .9):
             actions[i] = 'R'
-            if(start[0] == 49):
+            if(start[0] == 99):
                 sequence[i] = start
                 readings[i] = reading(graphs[j][start[0]][start[1]])
             elif(graphs[j][start[0]+1][start[1]] == 'B'):
@@ -102,9 +103,9 @@ def sequence(start, j):
                 start = ((start[0]+1),start[1])
         
         #south move
-        elif(move == 4):
+        elif(move == 4 and moveOdds <= .9):
             actions[i] = 'D'
-            if(start[1] == 99):
+            if(start[1] == 49):
                 sequence[i] = start
                 readings[i] = reading(graphs[j][start[0]][start[1]])
             elif(graphs[j][start[0]][start[1]+1] == 'B'):
@@ -119,6 +120,14 @@ def sequence(start, j):
         else:
             sequence[i] = start
             readings[i] = reading(graphs[j][start[0]][start[1]])
+            if(move == 1):
+                actions[i] = 'L'
+            elif(move == 2):
+                actions[i] = 'U'
+            elif(move == 3):
+                actions[i] = 'R'
+            else:
+                actions[i] = 'D'
 
     return sequence, actions, readings
 
@@ -133,27 +142,46 @@ def sequence(start, j):
 
 
 def start(i):
-    x = random.randint(0, 49)
-    y = random.randint(0, 99)
+    x = random.randint(0, 99)
+    y = random.randint(0, 49)
     while(graphs[i][x][y] == 'B'):
-        x = random.randint(0, 49)
-        y = random.randint(0, 99)
+        x = random.randint(0, 99)
+        y = random.randint(0, 49)
     return (x,y)
 
 def main():
+    # populate graphs array
     genGraph()
+    # write graphs
+    for i in range(10):
+        name = "graph" + str(i+1) + ".txt"
+        with open(name, "w") as f:
+            for y in range(50):
+                for x in range(100):
+                    f.write(str(graphs[i][x][y]) + " ")
+                f.write("\n")
+
+    # points to be used in simulations
+    points = [["" for x in range(10)] for y in range(10)]
+    # actions to be used in simulations
+    moves = [["" for x in range(10)] for y in range(10)]
     for j in range(10):
         for i in range(10):
+            # write ground truth files
             name = "groundTruthFiles\graph" + str(j+1) + "-" + str(i+1) + ".txt"
             with open(name, "w") as f:
                 startpoint = start(j)
                 path, actions, readings = sequence(startpoint, j)
+                points[j][i] = path
+                moves[j][i] = actions
                 path.insert(0,startpoint)
                 f.write(str(startpoint) + "\n")
                 f.write(str(path) + "\n")
                 f.write(str(actions) + "\n")
                 f.write(str(readings) + "\n")
-    graphics.graphics(graphs)
+        
+    # call graphics program
+    graphics.graphics(graphs, points, moves)
 
 
 main()
--- graphics.py
+++ graphics.py
@@ -1,37 +1,95 @@
 from tkinter import *
+import time
 
-def createGraph(root, graphNum, graphs):
+# create cells for graph
+def createGraph(root, graphNum, graphs, actions, points):
+    if 'my_canvas' in locals():
+        my_canvas.delete('all')
     graphNum = int(graphNum.get()) - 1
-    #if(graphNum > 10 or graphNum < 1):
-    #   print("no graph at that value")
-    #    root.withdraw()
-    #    graphics(graphs)
-    #else:
     my_canvas = Canvas(root, height=1500, width=1500, bg="white")
+
+    #control simulation buttons
+    label = Label(root, width=50, text="Select Ground Truth Path #1-10")
+    entry = Entry(root, width=50)
+    simOne = Button(root, width=20, text="Next Step", command= lambda: nextPoint(my_canvas, points, 1, entry, graphNum))
+    simTen = Button(root, width=20, text="Next 10 Steps", command= lambda: nextPoint(my_canvas, points, 10, entry, graphNum))
+    simEnd = Button(root, width=20, text="To End", command= lambda: nextPoint(my_canvas, points, 100, entry, graphNum))
+    label.pack()
+    entry.pack()
+    simOne.pack()
+    simTen.pack()
+    simEnd.pack()
+
     my_canvas.pack(pady=20)
+    
+    #create cells
     for x in range(100):
         for y in range(50):
-            #if(graphs[graphNum][x][y] == N):
             tempx = x + 20
             tempy = y + 10
-            if(graphs[graphNum][y][x] == 'H'):
+            if(x == 0 and y == 0):
+                print(tempx*10, tempy*10)
+            # fill cells with color based on type
+            if(graphs[graphNum][x][y] == 'H'):
                 my_canvas.create_rectangle(tempx*10, tempy*10, (tempx+1)*10, (tempy+1)*10, outline='grey', fill='green')
-            if(graphs[graphNum][y][x] == 'T'):
+            if(graphs[graphNum][x][y] == 'T'):
                 my_canvas.create_rectangle(tempx*10, tempy*10, (tempx+1)*10, (tempy+1)*10, outline='grey', fill='orange')
-            if(graphs[graphNum][y][x] == 'B'):
+            if(graphs[graphNum][x][y] == 'B'):
                 my_canvas.create_rectangle(tempx*10, tempy*10, (tempx+1)*10, (tempy+1)*10, outline='grey', fill='red')
             else:
                 my_canvas.create_rectangle(tempx*10, tempy*10, (tempx+1)*10, (tempy+1)*10, outline='grey')
+    
+    return
+    
 
-def graphics(graphs):
+counter = 0
+# simulate ground truth points
+def nextPoint(my_canvas, points, simStep, simNum, graphNum):
+    global counter
+    if not isinstance(simNum, int):
+        simNum = int(simNum.get()) - 1
+    if(simStep == 1):
+        x1 = ((points[graphNum][simNum][counter][0])+20.25)*10
+        y1 = ((points[graphNum][simNum][counter][1])+10.25)*10
+        x2 = ((points[graphNum][simNum][counter][0])+20.75)*10
+        y2 = ((points[graphNum][simNum][counter][1])+10.75)*10 
+        my_canvas.create_oval(x1, y1, x2, y2, fill='purple')
+        counter+=1
+        return
+    elif(simStep == 10):
+        temp = counter
+        while temp < counter+11 and temp < 100:
+            x1 = ((points[graphNum][simNum][temp][0])+20.25)*10
+            y1 = ((points[graphNum][simNum][temp][1])+10.25)*10
+            x2 = ((points[graphNum][simNum][temp][0])+20.75)*10
+            y2 = ((points[graphNum][simNum][temp][1])+10.75)*10 
+            my_canvas.create_oval(x1, y1, x2, y2, fill='purple')
+            temp+=1
+        counter+=10
+        return
+    elif(simStep == 100):
+        temp = counter
+        while temp < 100:
+            x1 = ((points[graphNum][simNum][temp][0])+20.25)*10
+            y1 = ((points[graphNum][simNum][temp][1])+10.25)*10
+            x2 = ((points[graphNum][simNum][temp][0])+20.75)*10
+            y2 = ((points[graphNum][simNum][temp][1])+10.75)*10 
+            my_canvas.create_oval(x1, y1, x2, y2, fill='purple')
+            temp+=1
+        counter+=100
+        return
+
+
+
+def graphics(graphs, points, actions):
     root = Tk()
     root.title('Part 5')
     root.geometry("500x500")
-    #creat entry and button for graph selection
+    #create entry and button for graph selection
     label = Label(root, width=100, text="normal: white, highway: green, hard to traverse: orange, blocked: red")
     label2 = Label(root, width=50, text="pick a graph file #1-10")
     graph = Entry(root, width=50)
-    button = Button(root, width=20, text="Create Graph", command= lambda: createGraph(root, graph, graphs))
+    button = Button(root, width=20, text="Create Graph", command= lambda: createGraph(root, graph, graphs, actions, points))
     label.pack()
     label2.pack()
     graph.pack()
